df46590ddbf2e94ace055f65063690fc7231439a
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-23 15:27:50 +1200

    tryit

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -89,26 +89,94 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
 
         });
 
-       
+        var $tryit;
+        var tCanvas;
+        var matrixData = [[1, 1, 1], [1,1,1], [1,1,1]];
+        var tButton;
 
-        var colorOrange4 = "#F0801A";
-        var colorOrange3 = "#FA8F00";
-        var colorOrange2 = "#FAA600";
-        var colorOrange1 = "#FABA00";
+        ext.set_console_process_ret(function (this_e, ret) {
+            try {
+                ret = JSON.parse(ret);
+            }
+            catch(err) {
+                $tryit.find(".checkio-result-in").html(JSON.stringify(ret));
+                tButton.attr("disabled", false);
+                return false
+            }
+            if (Array.isArray(ret) && ret.length == 3 && ret.every(function(x){return !isNaN(x)})) {
+                tCanvas.animateCanvas(matrixData, ret, false, tButton);
+            }
+            else {
+                tButton.attr("disabled", false);
+            }
+            $tryit.find(".checkio-result-in").html(JSON.stringify(ret));
+        });
 
-        var colorBlue4 = "#294270";
-        var colorBlue3 = "#006CA9";
-        var colorBlue2 = "#65A1CF";
-        var colorBlue1 = "#8FC7ED";
+        ext.set_generate_animation_panel(function (this_e) {
 
-        var colorGrey4 = "#737370";
-        var colorGrey3 = "#9D9E9E";
-        var colorGrey2 = "#C5C6C6";
-        var colorGrey1 = "#EBEDED";
+                $tryit = $(this_e.setHtmlTryIt(ext.get_template('tryit')));
+                tButton = $tryit.find(".bn-check");
+                tCanvas = new AmuletCanvas($tryit.find(".tryit-canvas")[0]);
+                tCanvas.createCanvas([0, 0, 0]);
+                var inputs = $tryit.find("table.matrix-in input");
+                inputs.keyup(function (e) {
+                    var $this = $(this);
+                    var value = $this.val();
+                    if (value.length > 2) {
+                        value = value.slice(0, 2);
+                        $this.val(value);
+                    }
+                    if (isNaN(value)) {
+                        value = "1";
+                        $this.val(value);
+                    }
+                    if (value.indexOf(".") !== -1) {
+                        value = value.slice(0, value.indexOf("."));
+                        $this.val(value);
+                    }
+                });
+                inputs.blur(function (e) {
+                    var $this = $(this);
+                    var value = $this.val();
+                    if (value === "") {
+                        $this.val(1);
+                    }
+                });
 
-        var colorWhite = "#FFFFFF";
+                tButton.click(function(e){
+
+                    inputs.each(function(index){
+                        var $this = $(this);
+                        matrixData[Math.floor(index / 3)][index % 3] = Number($this.val());
+                    });
+                    tButton.attr("disabled", true);
+                    tCanvas.resetCanvas();
+                    this_e.sendToConsoleCheckiO(matrixData);
+                    e.stopPropagation();
+                    return false;
+                });
+            }
+        );
+
 
         function AmuletCanvas(dom) {
+            var colorOrange4 = "#F0801A";
+            var colorOrange3 = "#FA8F00";
+            var colorOrange2 = "#FAA600";
+            var colorOrange1 = "#FABA00";
+
+            var colorBlue4 = "#294270";
+            var colorBlue3 = "#006CA9";
+            var colorBlue2 = "#65A1CF";
+            var colorBlue1 = "#8FC7ED";
+
+            var colorGrey4 = "#737370";
+            var colorGrey3 = "#9D9E9E";
+            var colorGrey2 = "#C5C6C6";
+            var colorGrey1 = "#EBEDED";
+
+            var colorWhite = "#FFFFFF";
+
             var radius = 50;
             var unit = Math.round(2 * radius / 3);
             var etalon = [0, 225, 315];
@@ -129,7 +197,7 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             var attrLeverCircle = {"stroke": colorBlue2, "stroke-width": radius / 5, "fill": colorBlue2};
             var attrAxis = {"stroke": colorGrey4, "opacity": 0.3};
 
-            this.createCanvas = function(start) {
+            this.createCanvas = function (start) {
                 paper.path(format("M{0},0V{1}", radius * 3, fullSize)).attr(attrAxis);
                 paper.path(format("M0,{0}H{1}", radius * 3, fullSize)).attr(attrAxis);
                 paper.path(format("M0,{0}L{0},0", fullSize)).attr(attrAxis);
@@ -144,11 +212,11 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
 //                shadow.attr("opacity", 0.5);
 
 
-                for (var i = 1; i <= 3; i++){
+                for (var i = 1; i <= 3; i++) {
                     var lev = paper.set();
                     lev.push(paper.path(format("M{0},{1}V{2}", cx, cx - radius, cx - radius - i * unit)).attr(attrLever));
                     lev.push(paper.circle(cx, cx - radius - i * unit, radius / 5).attr(attrLeverCircle));
-                    lev.rotate(start[i-1], cx, cx);
+                    lev.rotate(start[i - 1], cx, cx);
                     lev.angle = start[i - 1];
                     levers.push(lev);
                 }
@@ -157,10 +225,10 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
 
             };
 
-            this.animateCanvas = function(matrix, userAngles, recolor) {
-//                Number.prototype.mod = function(n) {
-//                    return ((this%n)+n)%n;
-//                };
+            this.animateCanvas = function (matrix, userAngles, recolor, button) {
+                function mod360 (n) {
+                    return ((n%360)+360)%360;
+                }
 
 
                 var delay = 15;
@@ -171,17 +239,24 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
 
                 var anglesSign = [angles[0] / userAngles[0], angles[1] / userAngles[1], angles[2] / userAngles[2]];
 
+                var etalon = function(ar) {
+                    return mod360(ar[0].angle) === 0 && mod360(ar[1].angle) === 225 && mod360(ar[2].angle) === 315;
+                };
+
                 function rotating() {
                     if (angles[i] <= 0) {
                         i++;
                         if (i >= 3) {
-                            if (recolor) {
+                            if (recolor || etalon(levers)) {
                                 levers.animate({"stroke": colorOrange1, "fill": colorOrange1}, 200);
                                 mainCircle.animate({"stroke": colorOrange1}, 200,
-                                    callback=function(){
+                                    callback = function () {
                                         levers.animate({"stroke": colorOrange4, "fill": colorOrange4}, 200);
                                         mainCircle.animate({"stroke": colorOrange4}, 200);
                                     });
+                            }
+                            if (button) {
+                                button.attr("disabled", false);
                             }
                             return false;
                         }
@@ -189,20 +264,8 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
                         return false;
                     }
 
-
                     var j = (i + 1) % 3,
                         k = (i + 2) % 3;
-
-//                    for (var l = 0; l < 3; l++) {
-//                        var an = ((levers[l].angle % 360) + 360) % 360;
-//                        if (an === etalon[l]) {
-//                            console.log(l);
-//                            levers[l].animate({"stroke": colorOrange4, "fill": colorOrange4}, 50);
-//                        }
-//                        else {
-//                            levers[l].animate({"stroke": colorBlue2, "fill": colorBlue2}, 50);
-//                        }
-//                    }
 
                     var sign = anglesSign[i];
                     levers[i].angle += sign;
@@ -212,11 +275,26 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
                     levers[j].animate({"transform": format("r{0},{1},{1}", levers[j].angle, cx)}, delay);
                     levers[k].animate({"transform": format("r{0},{1},{1}", levers[k].angle, cx)}, delay);
                     levers[i].animate({"transform": format("r{0},{1},{1}", levers[i].angle, cx)}, delay,
-                        callback=rotating);
+                        callback = rotating);
                 }
+
                 setTimeout(rotating, 500);
 
+            };
+
+            this.resetCanvas = function() {
+                for (var i = 0; i < 3; i++){
+                    levers[i].angle = 0;
+                    levers[i].animate({"transform": format("r{0},{1},{1}", levers[i].angle, cx)}, 300);
+                }
+                levers.animate({"stroke": colorBlue1, "fill": colorBlue1}, 200);
+                mainCircle.animate({"stroke": colorBlue1}, 200,
+                    callback = function () {
+                        levers.animate({"stroke": colorBlue2, "fill": colorBlue2}, 200);
+                        mainCircle.animate({"stroke": colorBlue2}, 200);
+                    });
             }
+
 
         }
 
